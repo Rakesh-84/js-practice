0c291db134912b730643791568cd2423cd78623a
rakesh-84 <aloerakesh.das@gmail.com>
2026-04-27 10:08:40 +0600

    update

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-async function getUserID(userid) {
-  if (typeof userid !== "number"){
-    throw new Error ("user id must be a number")
-
-  }
-try {
-  const response = await fetch (`https://jsonplaceholder.typicode.com/users/${userId}`)
-
-  if (!response.ok){
-    throw new Error ("server error" + response.status)
-  }
-  const data = await response.json()
-  console.log (data)
-
-} catch (error) {
-  console.log ("something went wrong", error.message)
+for (var i = 0; i < 3; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 100);
 }
-
-
-finally {console.log ("fetch complete")}
-
-
-}
-
