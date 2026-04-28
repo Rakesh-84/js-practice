92e94d0368ea942f51ab5dd4723533f360db97ed
rakesh-84 <aloerakesh.das@gmail.com>
2026-04-28 11:27:58 +0600

    update

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-for (let i = 0; i < 3; i++) {
-  setTimeout(function () {
-    console.log(i);
-  }, 100);
-}
+function fetchUserStats() {
+  return Promise.res("State loaded"),
+  .then (setTimeout((res) => {
+    
+  }, 1000);)
+.catch (error);
+  { console.log(error) }
+  .finally(console.log ("State loaded"))
