708c78aa9664dc1445cbf1bf31e6b1a96886af43
rakesh-84 <aloerakesh.das@gmail.com>
2026-04-28 11:39:48 +0600

    new update

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 function fetchUserStats() {
-  return Promise.res("State loaded"),
-  .then (setTimeout((res) => {
-    
-  }, 1000);)
-.catch (error);
-  { console.log(error) }
-  .finally(console.log ("State loaded"))
+  return
+  new Promise(resolve, reject) =>{
+    setTimeout(() =>
+    resolve("state uploaded"),1000)
+  }
+.then(() => {
+    console.log("state uploaded")
+  })
+    .catch((error) => {
+    console.error("Error fetching user stats:", error)
+  })
+  .finally(() => {
+    console.log("fetching user stats complete")
+  })
+}
