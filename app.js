7f7f75e5a0bc7ce6df5fb92c5718edb5b1142556
rakesh-84 <aloerakesh.das@gmail.com>
2026-05-02 12:32:30 +0600

    new update

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-function getUserID(token) {
-  if (token === "valid-token") {
-    return function userProfile() {
-      const profile = fetch ('/api/profile')
-        .then(res => res.json());
-      return profile;
-    }
-  }
+
+function getUser() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ name: "Rex", company: "KleanDash" });
+    }, 1000);
+  });
+}
+
+// Your task:
+// Write an async function called showUser
+// Use await to get the user from getUser()
+// Then console.log the user's name
+async function showUser() {
+  const user = await getUser();
+  console.log(user.name);
+}
+
+function getDashboards(company) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(["BizDash", "SalesDev", "OmniTask"]), 1000);
+  });
+}
+
+async function showDashboards() {
+  const user = await getUser();
+  const dashboards = await getDashboards(user.company);
+  console.log(dashboards);
+  console.log(user.name)
 }
