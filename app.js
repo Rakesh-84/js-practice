c6c61cf14b92f5d19ba410f94ebc318d7b9c0836
rakesh-84 <aloerakesh.das@gmail.com>
2026-05-11 21:17:56 +0600

    new update

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
 // 1. Grab all three elements
-const input = document.getElementById("metric-input");
-const btn = document.getElementById("add-card-btn");
-const container = document.getElementById("cards-container");
 
-// 2. Listen for button click
-btn.addEventListener("click", function() {
+const input1 = document.getElementById("input1")
+const button1 = document.getElementById("button1")
+const list1 = document.getElementById("list1")
 
-  // 3. Create a new div
-  const card = document.createElement("div");
+button1.addEventListener("click", function() {
+    const input = input1.value
+  const listItem = document.createElement("li")
+  listItem.textContent = input
+  list1.appendChild(listItem)
+  input1.value = ""
+}
+)
 
-  // 4. Set its text to whatever user typed
-  card.textContent = input.value;
-
-  // 5. Add class "kpi-card" to it
-  card.classList.add("kpi-card");
-
-  // 6. Append it to container
-  container.appendChild(card);
-
-  // 7. Clear the input
-  input.value = "";
-});
-
-const testInput = document.getElementById("test-input");
-const testBtn = document.getElementById("test-btn");
-const testList = document.getElementById("test-list");
-
-testBtn.addEventListener("click", function () {
-  const item = document.createElement("li");
-  item.textContent = testInput.value;
-  testList.appendChild(item);
-  testInput.value = "";
-})
-testList.addEventListener("click", function (event) {
-  if (event.target.tagName === "LI") {
-    event.target.remove();
-  }
+list1.addEventListener("click", function(event) {
+    if (event.target.tagName === "LI") {
+        event.target.remove()
+    }
 })
