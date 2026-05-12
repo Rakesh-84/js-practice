d4f27bd2a09f892f7affcbd814c4256e2797d8a5
rakesh-84 <aloerakesh.das@gmail.com>
2026-05-12 23:30:33 +0600

    new update

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 // 1. Grab all three elements
+const input = document.querySelector(".input");
+const button = document.querySelector(".button");
+const output = document.querySelector(".output");
+// 2. Add event listener to the button
+button.addEventListener("click", function () {
+  // 3. Get the value of the input
+  const inputValue = input.value;
+  // 4. Set the value of the output
+  output.value = inputValue;
+});
 
-const input1 = document.getElementById("input1")
-const button1 = document.getElementById("button1")
-const list1 = document.getElementById("list1")
-
-button1.addEventListener("click", function() {
-    const input = input1.value
-  const listItem = document.createElement("li")
-  listItem.textContent = input
-  list1.appendChild(listItem)
-  input1.value = ""
-}
-)
-
-list1.addEventListener("click", function(event) {
-    if (event.target.tagName === "LI") {
-        event.target.remove()
-    }
-})
