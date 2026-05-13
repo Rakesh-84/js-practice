3e54ff34ed6e5bc06d46daf106e792e3c3252e68
rakesh-84 <aloerakesh.das@gmail.com>
2026-05-13 11:38:09 +0600

    new update

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-const textWrite = document.getElementById("myTextarea")
-const wordCount = document.getElementById("counter")
-
-
-
-textWrite.addEventListener("input", function() {
-  countWords( );
-});
-
-// need to know about bowser event name
-function countWords() { 
-wordCount.textContent = textWrite.value.length + " / 200"
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.getElementById('submit');
   
-  if (textWrite.value.length < 100) {
-  return textWrite.style.color = "green" 
-  }
-  if (textWrite.value.length < 150) {
-    return textWrite.style.color = "yellow"
-  }
-  else {
-    return textWrite.style.color = "red"
-  }
-
-}
+  btn.addEventListener('click', function() {
+    console.log('clicked');
+  
+  });
+});
